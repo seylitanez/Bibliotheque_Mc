f14c23ed4d6b17a5e6078cfb35df4062b15ee630
seylitanez <lyes.zenati@se.univ-bejaia.dz>
2023-03-05 21:52:25 +0100

    j'ai modifie le titre de la fenetre FX

--- src/main/java/com/core/mcprojetbibliotheque/HelloApplication.java
+++ src/main/java/com/core/mcprojetbibliotheque/HelloApplication.java
@@ -1,23 +1,22 @@
 package com.core.mcprojetbibliotheque;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        scene.setFill(Color.TRANSPARENT);
-        stage.setTitle("Hello lyes!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
         stage.setScene(scene);
-        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
