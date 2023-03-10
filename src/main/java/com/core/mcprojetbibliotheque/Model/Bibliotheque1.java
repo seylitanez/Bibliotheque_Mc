7b96f72d83bf02ad3b82549b4995c5f4187f3792
adlal_kheraz <106343292+adlal-kheraz@users.noreply.github.com>
2023-03-10 19:11:36 +0100

    j'ai mie toutes les classes en commentaire pour les utiliser dans la creation des classes final

--- src/main/java/com/core/mcprojetbibliotheque/Model/Bibliotheque1.java
+++ src/main/java/com/core/mcprojetbibliotheque/Model/Bibliotheque1.java
@@ -2,14 +2,14 @@ package com.core.mcprojetbibliotheque.Model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bibliotheque {
-    private List<Livre> livres;
-    private List<Abonne> abonnes;
+public class Bibliotheque1 {
+  /*  private List<Livre> livres;
+    private List<Abonne1> abonne1s;
     private Gestionnaire gestionnaire;
 
-    public Bibliotheque(Gestionnaire gestionnaire) {
+    public Bibliotheque1(Gestionnaire gestionnaire) {
         this.livres = new ArrayList<>();
-        this.abonnes = new ArrayList<>();
+        this.abonne1s = new ArrayList<>();
         this.gestionnaire = gestionnaire;
     }
 
@@ -17,28 +17,28 @@ public class Bibliotheque {
         this.livres.add(livre);
     }
 
-    public void ajouterAbonne(Abonne abonne) {
-        this.abonnes.add(abonne);
+    public void ajouterAbonne(Abonne1 abonne1) {
+        this.abonne1s.add(abonne1);
     }
 
     public void retirerLivre(Livre livre) {
         this.livres.remove(livre);
     }
 
-    public void retirerAbonne(Abonne abonne) {
-        this.abonnes.remove(abonne);
+    public void retirerAbonne(Abonne1 abonne1) {
+        this.abonne1s.remove(abonne1);
     }
 
     public List<Livre> getLivres() {
         return this.livres;
     }
 
-    public List<Abonne> getAbonnes() {
-        return this.abonnes;
+    public List<Abonne1> getAbonnes() {
+        return this.abonne1s;
     }
 
     public Gestionnaire getGestionnaire() {
         return this.gestionnaire;
-    }
+    }*/
 }
 
