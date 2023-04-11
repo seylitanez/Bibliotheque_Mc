06801889b1f232b76023c79ae00c658c32ed5967
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-04-11 12:31:18 +0100

    update

--- src/main/java/com/core/mcprojetbibliotheque/Model/Connections.java
+++ src/main/java/com/core/mcprojetbibliotheque/Model/Connections.java
@@ -1,7 +1,30 @@
 package com.core.mcprojetbibliotheque.Model;
-import java.sql.Connection;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
 public class Connections {
-    Connection connection;
-    public Connections() {
+    private String login,password,url;
+    private Properties prop;
+    private Connection connection;
+
+    public Connections() throws Exception{
+        prop =new Properties();
+        try(FileInputStream fis=new FileInputStream("Conf.properties")){
+            prop.load(fis);
+        }
+    }
+    private void getProperties() throws Exception{
+        Class.forName(prop.getProperty("jdbc.driver.class"));
+        login=prop.getProperty("jdbc.login");
+        url=prop.getProperty("jdbc.url");
+        password=prop.getProperty("jdbc.password");
+    }
+    public void conection()throws Exception{
+        getProperties();
+        connection= DriverManager.getConnection(url,login,password);
+    }
+    public void desConection() throws Exception {
+        connection.close();
     }
 }
--- src/main/java/com/core/mcprojetbibliotheque/Utils/Constantes.java
+++ src/main/java/com/core/mcprojetbibliotheque/Utils/Constantes.java
@@ -1,7 +1,6 @@
 package com.core.mcprojetbibliotheque.Utils;
 
 public class Constantes {
-
     public static String AJOUT_UTILISATEUR="INSERT INTO `utilisateur` (`id`, `username`, `password`) VALUES (?, ?, ?);";
 
 }
