a1b255161fc3fa2854c9cd28b1282f03c0539e36
seylitanez <lyes.zenati@se.univ-bejaia.dz>
2023-04-14 20:47:19 +0200

    main chercher livre interface

--- src/main/java/com/core/mcprojetbibliotheque/Test.java
+++ src/main/java/com/core/mcprojetbibliotheque/Test.java
@@ -1,21 +1,23 @@
 package com.core.mcprojetbibliotheque;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 import java.io.IOException;
+
 public class Test extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Test.class.getResource("Test.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AdlalTest.class.getResource("AbonneDashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
-        stage.show();    }
-    public static void main(String[] args) {
-        launch();
+        stage.show();
     }
+
 }
