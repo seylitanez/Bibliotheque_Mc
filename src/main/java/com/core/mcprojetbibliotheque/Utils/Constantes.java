6f8deb3241a98272561dd88fc444516feade3204
seylitanez <lyes.zenati@se.univ-bejaia.dz>
2023-04-15 18:35:32 +0200

    modification de la requete sql car j'ai ajoute une colonne

--- src/main/java/com/core/mcprojetbibliotheque/Utils/Constantes.java
+++ src/main/java/com/core/mcprojetbibliotheque/Utils/Constantes.java
@@ -1,6 +1,6 @@
 package com.core.mcprojetbibliotheque.Utils;
 
 public class Constantes {
-    public static String AJOUT_UTILISATEUR="INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `username`, `password`, `email`, `categorie`,`certificat`) VALUES (null, ?, ?,?, ?, ?,?,?);";
+    public static String AJOUT_UTILISATEUR="INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `username`, `password`, `email`, `categorie`,`certificat`,`valide`) VALUES (null, ?, ?,?, ?, ?,?,?,0);";
     public static String TROUVE_UTILISATEUR="SELECT  `id`, `nom`, `prenom`, `username`, `password`, `email`, `categorie`,`certificat` FROM `utilisateur` WHERE `username`= ? AND `password`= ? ;";
 }
