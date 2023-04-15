f8d4c92bf8ffc542509530ec93282b56b69779f2
seylitanez <lyes.zenati@se.univ-bejaia.dz>
2023-04-15 18:19:31 +0200

    correction model Enseignant Etudiant

--- src/main/java/com/core/mcprojetbibliotheque/Model/Etudiant.java
+++ src/main/java/com/core/mcprojetbibliotheque/Model/Etudiant.java
@@ -1,12 +1,18 @@
 package com.core.mcprojetbibliotheque.Model;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Etudiant extends Abonne {
-    public Etudiant(String nom, String prenom, String username, String password, String email, String categorie, Date dateInscription, Date dateEmpunt, ArrayList<Livre> empreunts, boolean penalite, boolean compteValide) {
-        super(nom, prenom, username, password, email, categorie, dateInscription, dateEmpunt, empreunts, penalite, compteValide);
+    public Etudiant(String nom, String prenom, String username, String password, String email, String categorie, Date dateInscription, Date dateEmpunt, File certificat, ArrayList<Livre> empreunts, boolean penalite, boolean compteValide) {
+        super(nom, prenom, username, password, email, categorie, dateInscription, dateEmpunt, certificat, empreunts, penalite, compteValide);
     }
+
+    public Etudiant(String nom, String prenom, String username, String password, String email, String categorie, Date dateInscription, File certificat) {
+        super(nom, prenom, username, password, email, categorie, dateInscription, certificat);
+    }
+
     @Override
     public boolean peutEmprunter() {
         return false;
